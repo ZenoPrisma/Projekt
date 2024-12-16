a85cc628fa24006cff7a130984a700bb1848a4da
NEXUS\Yannick.Ackermann <Yannick.Ackermann@nexus-ag.de>
2024-12-16 11:04:25 +0100

    Added {"--------"} to Personlist (TestCommit)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { Person } from "./Person";
 
 function App() {
 
-  const testUser = () => ({ name: "Pedro", age: 25, isMarried: true})
+  const testUser = () => ({ name: "Pedro", age: 25, isMarried: true});
   const testedUser = testUser();
 
   return (
     <>
+      {"--------"}
       <Person name={"Henry"} age={22} isMarried={false}/>{""}
       {"--------"}
       <Person name={"Paul"} age={34} isMarried={true}/>{""}
